8f3670546891270510d421da0e729f30c41071bb
darina78945612300-ux <darina78945612300@gmail.com>
2025-10-04 11:40:32 +0300

    Замена сложения на вычитание через web-сервис

--- program.sh
+++ program.sh
@@ -4,5 +4,5 @@ echo "Введите первое целое число:"
 read num1
 echo "Введите второе целое число:"
 read num2
-result=$((num1+num2))
-echo "Сумма чисел: $num1 + $num2 = $result"
+result=$((num1 - num2))
+echo "Сумма чисел: $num1 - $num2 = $result"
